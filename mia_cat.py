2018263e36a951802c3d16f81d97d17aa95bfd5e
ikuma <namashin.xxx@icloud.com>
2024-04-17 22:05:16 +0800

    support for MaxOS and Windows

--- mia_cat.py
+++ mia_cat.py
@@ -1,166 +1,149 @@
 import asyncio
 import configparser
-import os
+import contextlib
+import platform
 import subprocess
 import threading
-from typing import List, Optional
+from typing import List
 
 import psutil
 import pystray
-import win32api
-import win32event
 from PIL import Image
-from win32.lib import winerror
 
+import utils
 from logger import Logger
 
-INI_FILE_PATH = "./ini/settings.ini"
-LOG_PATH = "./log/mia_cat"
+IS_WINDOWS = False
+if platform.system() == "Windows":
+    IS_WINDOWS = True
 
-# make logger
-logger = Logger(LOG_PATH)
+    from win32.lib import winerror
+    import win32api
+    import win32event
 
-# white cat icon
-WHITE_CAT_ICONS = [Image.open(f"./res/cat/white_cat_{i}.ico") for i in range(5)]
-# black cat icon
-BLACK_CAT_ICONS = [Image.open(f"./res/cat/black_cat_{i}.ico") for i in range(5)]
-# white horse icon
-WHITE_HORSE_ICONS = [Image.open(f"./res/horse/white_horse_{i}.ico") for i in range(14)]
-# black horse icon
-BLACK_HORSE_ICONS = [Image.open(f"./res/horse/black_horse_{i}.ico") for i in range(14)]
-# white parrot icon
-WHITE_PARROT_ICONS = [Image.open(f"./res/parrot/white_parrot_{i}.ico") for i in range(10)]
-# black parrot icon
-BLACK_PARROT_ICONS = [Image.open(f"./res/parrot/black_parrot_{i}.ico") for i in range(10)]
+logger = Logger("./log/mia_cat")
 
 RUNNING_ANIMALS_MAPPING = {
-    "white_cat": WHITE_CAT_ICONS,
-    "black_cat": BLACK_CAT_ICONS,
-    "white_horse": WHITE_HORSE_ICONS,
-    "black_horse": BLACK_HORSE_ICONS,
-    "white_parrot": WHITE_PARROT_ICONS,
-    "black_parrot": BLACK_PARROT_ICONS,
+    "white_cat": [Image.open(f"./res/cat/white_cat_{i}.png") for i in range(5)],
+    "black_cat": [Image.open(f"./res/cat/black_cat_{i}.png") for i in range(5)],
+    "white_horse": [Image.open(f"./res/horse/white_horse_{i}.png") for i in range(14)],
+    "black_horse": [Image.open(f"./res/horse/black_horse_{i}.png") for i in range(14)],
+    "white_parrot": [Image.open(f"./res/parrot/white_parrot_{i}.png") for i in range(10)],
+    "black_parrot": [Image.open(f"./res/parrot/black_parrot_{i}.png") for i in range(10)],
 }
 
-
-def find_exe_abspath(exe_name: str) -> Optional[str]:
-    for root, dirs, files in os.walk(os.environ["ProgramFiles"]):
-        if exe_name in files:
-            return os.path.join(root, exe_name)
-
-    for root, dirs, files in os.walk(os.environ["ProgramFiles(x86)"]):
-        if exe_name in files:
-            return os.path.join(root, exe_name)
-
-    return None
-
-
-# TODO You can add new application you want
-APP_PATH = {
-    "wechat": find_exe_abspath("WeChat.exe"),
-    "qq": find_exe_abspath("QQ.exe"),
-    "chrome": find_exe_abspath("chrome.exe"),
-}
+if IS_WINDOWS:
+    WINDOWS_APP_PATH = {
+        "wechat": utils.find_windows_abspath("WeChat.exe"),
+        "qq": utils.find_windows_abspath("QQ.exe"),
+        "chrome": utils.find_windows_abspath("chrome.exe"),
+    }
+else:
+    MAC_APP_PATH = {
+        "wechat": utils.find_mac_abspath("WeChat.app"),
+        "qq": utils.find_mac_abspath("QQ.app"),
+        "chrome": utils.find_mac_abspath("Google Chrome.app")
+    }
 
 
 class MiaCat(object):
     def __init__(self, running_animal: str) -> None:
         self.running_animal = running_animal
         self.running_animal_icons = RUNNING_ANIMALS_MAPPING[running_animal]
-        self.is_running = False
         self.even_loop = None
         self.mia_cat_mutex = None
-
-        # animal menus
-        cat_menu = pystray.Menu(
-            pystray.MenuItem(
-                "White Cat",
-                lambda icon, item: self.change_running_animal("white_cat", WHITE_CAT_ICONS),),
-            pystray.MenuItem(
-                "Black Cat",
-                lambda icon, item: self.change_running_animal("black_cat", BLACK_CAT_ICONS),),
-        )
-        horse_menu = pystray.Menu(
-            pystray.MenuItem(
-                "White Horse",
-                lambda icon, item: self.change_running_animal("white_horse", WHITE_HORSE_ICONS),),
-            pystray.MenuItem(
-                "Black Horse",
-                lambda icon, item: self.change_running_animal("black_horse", BLACK_HORSE_ICONS),),
-        )
-        parrot_menu = pystray.Menu(
-            pystray.MenuItem(
-                "White Parrot",
-                lambda icon, item: self.change_running_animal("white_parrot", WHITE_PARROT_ICONS),),
-            pystray.MenuItem(
-                "Black Parrot",
-                lambda icon, item: self.change_running_animal("black_parrot", BLACK_PARROT_ICONS),),
-        )
-        # sub menus
-        animal_menus = pystray.Menu(
-            pystray.MenuItem("Cat", cat_menu),
-            pystray.MenuItem("Horse", horse_menu),
-            pystray.MenuItem("Parrot", parrot_menu),
-        )
-
-        # application menus
-        app_menus = pystray.Menu(
-            pystray.MenuItem("WeChat", lambda icon, item: self.start_exe("wechat")),
-            pystray.MenuItem("QQ", lambda icon, item: self.start_exe("qq")),
-            pystray.MenuItem("Chrome", lambda icon, item: self.start_exe("chrome")),
-        )
-
-        # main menus
-        main_menus = pystray.Menu(
-            pystray.MenuItem("Animals", animal_menus),
-            pystray.MenuItem("Apps", app_menus),
-            pystray.MenuItem("Exit", self.stop_mia_cat),
-        )
 
         self.mia_cat = pystray.Icon(
             name="MiaCat",
             title="CPU : " + str(self.get_cpu_percent()),
             icon=self.running_animal_icons[0],
-            menu=main_menus,
+            menu=self.make_menu(),
         )
 
-    def start_mia_cat(self) -> None:
-        # ? already launched
-        self.mia_cat_mutex = win32event.CreateMutex(None, 0, "mia_cat_mutex")
-        if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
-            logger.error("MiaCat is running. Exiting.")
-            return
+    def make_menu(self) -> pystray.Menu:
+        cat_menu = pystray.Menu(
+            pystray.MenuItem(
+                "While Cat",
+                lambda icon, item: self.change_running_animal("white_cat",
+                                                              RUNNING_ANIMALS_MAPPING["white_cat"]),
+            ),
+            pystray.MenuItem(
+                "Black Cat",
+                lambda icon, item: self.change_running_animal("black_cat",
+                                                              RUNNING_ANIMALS_MAPPING["black_cat"]), ),
+        )
 
-        self.is_running = True
+        horse_menu = pystray.Menu(
+            pystray.MenuItem(
+                "White Horse",
+                lambda icon, item: self.change_running_animal("white_horse",
+                                                              RUNNING_ANIMALS_MAPPING["white_horse"]), ),
+            pystray.MenuItem(
+                "Black Horse",
+                lambda icon, item: self.change_running_animal("black_horse",
+                                                              RUNNING_ANIMALS_MAPPING["black_horse"]), ),
+        )
+
+        parrot_menu = pystray.Menu(
+            pystray.MenuItem(
+                "White Parrot",
+                lambda icon, item: self.change_running_animal("white_parrot",
+                                                              RUNNING_ANIMALS_MAPPING["white_parrot"]), ),
+            pystray.MenuItem(
+                "Black Parrot",
+                lambda icon, item: self.change_running_animal("black_parrot",
+                                                              RUNNING_ANIMALS_MAPPING["black_parrot"]), ),
+        )
+
+        # animal menus
+        animal_menus = pystray.Menu(
+            pystray.MenuItem("Cat", cat_menu),
+            pystray.MenuItem("Horse", horse_menu),
+            pystray.MenuItem("Parrot", parrot_menu)
+        )
+
+        # application menus
+        mac_app_menus = pystray.Menu(
+            pystray.MenuItem("WeChat", lambda icon, item: self.launch_app("wechat")),
+            pystray.MenuItem("QQ", lambda icon, item: self.launch_app("qq")),
+            pystray.MenuItem("Chrome", lambda icon, item: self.launch_app("chrome")),
+        )
+
+        # main menus
+        main_menus = pystray.Menu(
+            pystray.MenuItem("Animals", animal_menus),
+            pystray.MenuItem("Apps", mac_app_menus),
+            pystray.MenuItem("Exit", self.stop_mia_cat),
+        )
+
+        return main_menus
+
+    def start_mia_cat(self) -> None:
+        if self.is_app_running():
+            return
 
         self.even_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.even_loop)
 
         main_thread = threading.Thread(target=lambda: asyncio.run(self.start_running()))
+        main_thread.daemon = True
         main_thread.start()
 
         self.mia_cat.run()
 
-        logger.info(f"Start MiaCat Application")
-        main_thread.join()
-
     def stop_mia_cat(self) -> None:
-        self.is_running = False
         self.even_loop.stop()
         self.mia_cat.stop()
 
-        logger.info(f"Stop MiaCat Application")
-
-        # update current run animal
         config.set("animal", "run_animal", self.running_animal)
         with open(INI_FILE_PATH, "w") as cf:
             config.write(cf)
 
-        # release the mutex
-        win32api.CloseHandle(self.mia_cat_mutex)
+        if IS_WINDOWS:
+            win32api.CloseHandle(self.mia_cat_mutex)
 
     async def start_running(self) -> None:
-        while self.is_running:
+        while True:
             await self.run_animal()
 
     async def run_animal(self) -> None:
@@ -175,30 +158,47 @@ class MiaCat(object):
             self.mia_cat.icon = animal_icon
 
     def change_running_animal(self, new_running_animal: str, new_animal_icons: List[Image.Image]) -> None:
-        logger.info(f"Animal switched from {self.running_animal} to {new_running_animal}")
-
         self.running_animal = new_running_animal
         self.running_animal_icons = new_animal_icons
-        self.mia_cat.icon = self.running_animal_icons[0]
 
-    def start_exe(self, app_name: str) -> None:
-        if APP_PATH[app_name] is None:
+    def launch_app(self, app_name: str) -> None:
+        if IS_WINDOWS:
+            app_path = WINDOWS_APP_PATH[app_name]
+        else:
+            app_path = MAC_APP_PATH[app_name]
+
+        if app_path is None:
             logger.error(f'Application name "{app_name}" is called, but could not find path')
             return
 
-        try:
-            subprocess.Popen([APP_PATH[app_name]])
-            logger.info(f"{app_name} started as subprocess")
-        except subprocess.CalledProcessError as ex:
-            logger.error(f"Subprocess failed with return code {ex.returncode}. Output: {ex.output}")
-        except Exception as ex:
-            logger.error(f"Error during subprocess execution: {ex}")
+        proc = subprocess.Popen(["open", "-a", app_path])
+        return_code = proc.wait()
+
+        if proc.returncode != 0:
+            logger.error(f"Application {MAC_APP_PATH[app_name]} failed to start with return code {return_code}.")
 
     def get_cpu_percent(self) -> float:
         return psutil.cpu_percent()
 
+    def is_app_running(self) -> bool:
+        if IS_WINDOWS:
+            self.mia_cat_mutex = win32event.CreateMutex(None, 0, "mia_cat_mutex")
+            if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+                logger.info(f"MiaCat is already running. Exiting.")
+                return True
+            return False
+
+        with contextlib.suppress(psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            for proc in psutil.process_iter():
+                if "python" in proc.name() and "mia_cat.py" in proc.cmdline():
+                    logger.info(f"MiaCat is already running. Exiting.")
+                    return True
+        return False
+
 
 if __name__ == "__main__":
+    INI_FILE_PATH = "./ini/settings.ini"
+
     config = configparser.ConfigParser()
     config.read(INI_FILE_PATH)
 
---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+from typing import Optional
+
+
+def find_windows_abspath(exe_name: str) -> Optional[str]:
+    for root, dirs, files in os.walk(os.environ["ProgramFiles"]):
+        if exe_name in files:
+            return os.path.join(root, exe_name)
+
+    for root, dirs, files in os.walk(os.environ["ProgramFiles(x86)"]):
+        if exe_name in files:
+            return os.path.join(root, exe_name)
+
+    return None
+
+
+def find_mac_abspath(app_name):
+    for root, dirs, _ in os.walk('/Applications'):
+        if app_name in dirs:
+            return os.path.join(root, app_name)
+    return None
